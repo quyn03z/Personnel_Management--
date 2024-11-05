f8b6a177a453e30ce36d6c0f9e084fe3f47aba1e
quyn03z <quyenvn03@gmail.com>
2024-11-05 11:21:41 +0700

    commit new

---
+++ Personnel_Management/Personnel_Management.Data/BaseRepository/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel_Management.Data.BaseRepository
+{
+	public class BaseRepository<T> : IBaseRepository<T> where T : class
+	{
+		public Task AddAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteAsync(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetByEmailAsync(string email)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetByIdAsync(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Personnel_Management/Personnel_Management.Data/BaseRepository/IBaseRepository.cs
+++ Personnel_Management/Personnel_Management.Data/BaseRepository/IBaseRepository.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Personnel_Management.Data.BaseRepository
 {
-	internal interface IBaseRepository
+	public interface IBaseRepository<T> where T : class
 	{
+		Task<T> GetByIdAsync(Guid id);
+		Task<T> GetByEmailAsync(string email);
+
+		Task AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(Guid id);
+
+		IEnumerable<T> GetAll();
 	}
 }
