0fa5912e50b8c6f1a0bfc2840846cf71bf46b381
HoangHHHE171821 <hoanghhhe171821@fpt.edu.vn>
2024-11-05 13:40:57 +0700

    Add NhanVienService

--- Personnel_Management/Personnel_Management.Business/NhanVienService/NhanVienService.cs
+++ Personnel_Management/Personnel_Management.Business/NhanVienService/NhanVienService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Personnel_Management.Data.EntityRepository;
+using Personnel_Management.Models.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,54 @@ using System.Threading.Tasks;
 
 namespace Personnel_Management.Business.NhanVienService
 {
-	internal class NhanVienService
-	{
-	}
+    public class NhanVienService : INhanVienService
+    {
+        private readonly INhanVienRepository _nhanVienRepository;
+
+        public NhanVienService(INhanVienRepository nhanVienRepository)
+        {
+            _nhanVienRepository = nhanVienRepository;
+        }
+
+        public async Task<IEnumerable<NhanVien>> GetAllAsync()
+        {
+            return await _nhanVienRepository.GetAllNhanViensAsync();
+        }
+
+        public async Task<NhanVien?> GetByIdAsync(int id)
+        {
+            return await _nhanVienRepository.GetNhanVienByIdAsync(id);
+        }
+
+        public async Task<NhanVien> AddAsync(NhanVien nhanVien)
+        {
+            await _nhanVienRepository.AddNhanVienAsync(nhanVien);
+            return nhanVien;
+        }
+
+        //public async Task UpdateAsync(NhanVien nhanVien)
+        //{
+        //    await _nhanVienRepository.UpdateAsync(nhanVien);
+        //}
+
+        //public async Task DeleteAsync(int id)
+        //{
+        //    await _nhanVienRepository.DeleteAsync(id);
+        //}
+
+        //public async Task<NhanVien?> GetByEmailAsync(string email)
+        //{
+        //    return await _nhanVienRepository.GetByEmailAsync(email);
+        //}
+
+        public NhanVien Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateJwtToken(NhanVien user)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
