630174469d88597ca464caa17ef48ab127b032fd
Anhpqdhe172622 <anhpqdhe172622@fpt.edu.vn>
2024-11-06 23:08:03 +0700

    add datatables

--- Personnel_Management/Personnel_Management.Client/src/views/dashboard/dashboard.component.ts
+++ Personnel_Management/Personnel_Management.Client/src/views/dashboard/dashboard.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { NavbarComponent } from "../../layout/navbar/navbar.component";
 import { SidebarComponent } from "../../layout/sidebar/sidebar.component";
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [NavbarComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Khởi tạo DataTable
+    ($('#dataTable') as any).DataTable();
+  }
 }
