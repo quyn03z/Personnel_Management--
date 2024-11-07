18a9bdd00c2a629f93f646a1c6bd89c8789c41f1
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-11-07 13:17:20 +0700

    DTO

---
+++ Personnel_Management/Personnel_Management.Models/DTO/DepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DepartmentDto
+{
+    public string TenPhongBan { get; set; }
+    public string MoTa { get; set; }
+}
