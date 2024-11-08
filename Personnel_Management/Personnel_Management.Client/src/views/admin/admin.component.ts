b2c55a776a2efe8a5a16bd11f7850f07143a3def
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-11-08 10:53:23 +0700

    getallphongban

--- Personnel_Management/Personnel_Management.Client/src/views/admin/admin.component.ts
+++ Personnel_Management/Personnel_Management.Client/src/views/admin/admin.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { DataTablesModule } from 'angular-datatables';
+import { Config } from 'datatables.net';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -7,6 +14,24 @@ import { Component } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
-
+export class AdminComponent implements OnInit {
+  departmentList: any[]= [];
+  http = inject(HttpClient);
+  dtoptions: Config={};
+  dttrigger: Subject<any> = new Subject<any>(); 
+  ngOnInit(): void {
+    this.getAllDepartment();
+    this.dtoptions={
+      pagingType: 'full_numbers',
+      lengthMenu:[5,8,15,20],
+      pageLength: 5
+    }
+  }
+  getAllDepartment() {
+    this.http.get("https://localhost:7182/api/Department").subscribe((res:any) =>{
+      this.departmentList = res.$values;
+      console.log(this.departmentList); 
+      this.dttrigger.next(null);  
+    })
+  }
 }
