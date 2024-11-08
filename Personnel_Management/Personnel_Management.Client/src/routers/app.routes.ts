914bc6796f20f9c9488c3ed7503010fe3e424b6e
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-11-08 09:53:29 +0700

    Update app.routes.ts

--- Personnel_Management/Personnel_Management.Client/src/routers/app.routes.ts
+++ Personnel_Management/Personnel_Management.Client/src/routers/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ViewEmployeeListComponent } from '../views/view-employee-list/view-employee-list.component';
 import { LoginComponent } from '../views/login/login.component';
+import { AdminComponent } from '../views/admin/admin.component';
 
 export const routes: Routes = [
     {
@@ -17,6 +18,11 @@ export const routes: Routes = [
         path: 'viewEmployeeList',
         title: 'Xem Danh Sach Nhan Vien',
         component: ViewEmployeeListComponent
+    },
+    {
+        path:'admin',
+        title:'admin',
+        component: AdminComponent
     }
 
 ];
