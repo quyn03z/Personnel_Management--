c0776d1d0b86702390c3fc832d1fa064cef1f187
minhkhai10122003 <136252548+khaioday@users.noreply.github.com>
2024-11-08 12:11:27 +0700

    fix deparment

---
+++ Personnel_Management/Personnel_Management.Business/DepartmentService/IDepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using Personnel_Management.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IDepartmentService
+{
+    Task<List<PhongBan>> GetAllDepartmentsAsync();
+    Task<PhongBan> GetDepartmentByIdAsync(int id);
+    Task AddDepartmentAsync(DepartmentDto departmentDto);
+    Task UpdateDepartmentAsync(int id, DepartmentDto departmentDto);
+    Task DeleteDepartmentAsync(int id);
+}
---
+++ Personnel_Management/Personnel_Management.Data/DepartmentRepository/IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Personnel_Management.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IDepartmentRepository
+{
+    Task<List<PhongBan>> GetAllDepartmentsAsync();
+    Task<PhongBan> GetDepartmentByIdAsync(int id);
+    Task AddDepartmentAsync(PhongBan department);
+    Task UpdateDepartmentAsync(PhongBan department);
+    Task DeleteDepartmentAsync(PhongBan department);
+}
