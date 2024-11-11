14886e9a5cadea4dc14474ca174b65c913e2f999
HoangHHHE171821 <hoanghhhe171821@fpt.edu.vn>
2024-11-11 10:19:08 +0700

    fix db

--- Personnel_Management/Personnel_Management.Models/Models/NhanVien.cs
+++ Personnel_Management/Personnel_Management.Models/Models/NhanVien.cs
@@ -24,6 +24,7 @@ public partial class NhanVien
     public int PhongBanId { get; set; }
 
     public string Matkhau { get; set; }
+    public string Avatar { get; set; }
 
     public bool IsManager => RoleId == 2; // Assume RoleId=2 is Manager
 
