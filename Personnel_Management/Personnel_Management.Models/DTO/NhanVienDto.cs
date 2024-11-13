a8af34b5a8697a2dc61d6bfe543a4fc21284b337
HoangHHHE171821 <hoanghhhe171821@fpt.edu.vn>
2024-11-13 12:55:59 +0700

    ban and unban Nhanvien

--- Personnel_Management/Personnel_Management.Models/DTO/NhanVienDto.cs
+++ Personnel_Management/Personnel_Management.Models/DTO/NhanVienDto.cs
@@ -16,6 +16,7 @@ namespace Personnel_Management.Models.DTO
         public string Email { get; set; }
         public int RoleId { get; set; }
         public int PhongBanId { get; set; }
+        public bool isBanned { get; set; }
  
         public DepartmentDto PhongBan { get; set; }
     }
