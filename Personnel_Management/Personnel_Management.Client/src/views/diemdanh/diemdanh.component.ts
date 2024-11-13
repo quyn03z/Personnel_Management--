77fbd3873eb007f8f05d411b9c83a66d7e03a408
quyn03z <quyenvn03@gmail.com>
2024-11-13 10:23:26 +0700

    add teamplate diemdanh

--- Personnel_Management/Personnel_Management.Client/src/views/diemdanh/diemdanh.component.ts
+++ Personnel_Management/Personnel_Management.Client/src/views/diemdanh/diemdanh.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-diemdanh',
   standalone: true,
-  imports: [],
+  imports: [MatDatepickerModule,CommonModule,MatInputModule,MatNativeDateModule],
   templateUrl: './diemdanh.component.html',
-  styleUrl: './diemdanh.component.scss'
+  styleUrls: ['./diemdanh.component.scss'],
 })
 export class DiemdanhComponent {
+  selectedDate: Date | null = null;
 
+  onDateSelected(event: Date): void {
+    this.selectedDate = event;
+  }
 }
